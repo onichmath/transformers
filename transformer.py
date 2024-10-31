6b38764388f91a2e68c14b34a0369e7fbbff5c94
Matthew O <mathonich@yahoo.com>
2024-10-30 20:36:38 -0700

    Hyperparameters

--- transformer.py
+++ transformer.py
@@ -1,1 +1,32 @@
 # add all  your Encoder and Decoder code here
+import torch
+import torch.nn as nn
+from torch.nn import functional as F 
+
+batch_size = 16  # Number of independent sequences  we will process in parallel
+block_size = 32  # Maximum context length for predictions
+learning_rate = 1e-3  # Learning rate for the optimizer
+n_embd = 64  # Embedding dimension
+n_head = 2  # Number of attention heads
+n_layer = 4  # Number of transformer layers
+
+
+eval_interval = 100  # How often to evaluate train and test perplexity during training
+max_iters = 500 # For language modeling, we can process all the batches for the entire dataset, but that takes a while, so we'll limit it to 500 iterations. For batch size of 16 and block size of  32, this is roughly, this is  500 * 16 * 32 = 256000 tokens, SOTA LMs are trained on trillions of tokens, so this is a very small dataset.
+eval_iters = 200  # Number of iterations to evaluate perplexity on the test set
+
+
+## classifier training hyperparameters. It is a simple 1 hidden layer feedforward network, with input 
+## size of 64, hidden size of 50 and output size of 3.
+
+n_input = 64  # Input size for the classifier, should match the embedding size of the transformer
+n_hidden = 100  # Hidden size for the classifier
+n_output = 3  # Output size for the classifier, we have 3 classes
+epochs_CLS = 15 # epochs for classifier training
+
+class Encoder(nn.Module):
+    # Batch x Time x Channel 
+    # logs.shape = B x T x C
+    # crossentropyLoss needs B x C x T
+    def __init__(self):
+        super().__init__()
